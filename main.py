4ab7eacd86f3f8a45683adb6a2b87e9365281644
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-05 10:21:38 +0000

    Declarar la lista y la tupla, imprimirlas e imprimir un elemento elegido de ambas

---
+++ main.py
@@ -0,0 +1,14 @@
+#Ejercicio 1
+coches = ["Mercedes", "Audi", "BMW", "Porsche", "Volkswagen"]
+motos = ("Ducatti", "Kawasaki", "Harley", "Honda", "Yamaha")
+print(coches)
+print(motos)
+print(coches[1])
+print(motos[3])
+
+
+
+
+
+
+
